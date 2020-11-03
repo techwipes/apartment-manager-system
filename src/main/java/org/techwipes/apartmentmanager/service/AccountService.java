190dd3cffb3c9c17c9a5c9f544065c00a925c843
Mitya <techwipes@gmail.com>
2020-11-03 22:28:43 +0500

    add init settings

---
+++ src/main/java/org/techwipes/apartmentmanager/service/AccountService.java
@@ -0,0 +1,9 @@
+package org.techwipes.apartmentmanager.service;
+
+import org.techwipes.apartmentmanager.model.Account;
+
+import java.util.List;
+
+public interface AccountService {
+    List<Account> getAllAccounts();
+}
---
+++ src/main/java/org/techwipes/apartmentmanager/service/AccountServiceImpl.java
@@ -0,0 +1,19 @@
+package org.techwipes.apartmentmanager.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.techwipes.apartmentmanager.model.Account;
+import org.techwipes.apartmentmanager.repository.AccountRepository;
+
+import java.util.List;
+
+@Service
+public class AccountServiceImpl implements AccountService {
+    @Autowired
+    private AccountRepository accountRepository;
+
+    @Override
+    public List<Account> getAllAccounts() {
+        return accountRepository.findAll();
+    }
+}
