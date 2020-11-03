9a694f2d0c5927f104242e69bd138f2fef244d93
Mitya <techwipes@gmail.com>
2020-11-03 20:47:58 +0500

    first init

---
+++ src/main/java/org/techwipes/apartmentmanager/ApartmentManagerApplication.java
@@ -0,0 +1,13 @@
+package org.techwipes.apartmentmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApartmentManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApartmentManagerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/techwipes/apartmentmanager/ApartmentManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.techwipes.apartmentmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApartmentManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
