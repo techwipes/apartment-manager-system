0c79799d6c439b13c54037f1846970bee00b1880
Mitya <techwipes@gmail.com>
2020-11-06 16:54:06 +0500

    add formUpdate handler

--- src/main/java/org/techwipes/apartmentmanager/controller/AccountController.java
+++ src/main/java/org/techwipes/apartmentmanager/controller/AccountController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.techwipes.apartmentmanager.model.Account;
 import org.techwipes.apartmentmanager.service.AccountServiceImpl;
@@ -18,22 +19,36 @@ public class AccountController {
 
     //display list of accounts
     @GetMapping("/")
-    public String viewHomePage(Model model){
+    public String viewHomePage(Model model) {
         model.addAttribute("listAccounts", accountService.getAllAccounts());
         return "index";
     }
+
     @GetMapping("/showNewAccountForm")
-    public String showNewAccountForm(Model model){
+    public String showNewAccountForm(Model model) {
         // create model attribute to bind form data
         Account account = new Account();
         model.addAttribute("account", account);
         return "new_account";
     }
+
     @PostMapping("/saveAccount")
-    public String  saveAccount(@ModelAttribute("account") Account account){
+    public String saveAccount(@ModelAttribute("account") Account account) {
         //save account to db
         accountService.saveAccount(account);
         return "redirect:/";
+    }
+
+    @GetMapping("/showFormForUpdate/{id}")
+    public String showFormForUpdate(@PathVariable(value = "id") long id, Model model) {
+       //get account from the service
+        Account account = accountService.getAccountById(id);
+
+        //set account object to the model
+        model.addAttribute("account", account);
+        return "update_employee";
 
     }
+
+
 }
