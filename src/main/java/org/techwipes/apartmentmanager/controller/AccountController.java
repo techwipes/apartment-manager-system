e6680bef4a20f5a7926c3ed7686aee2cf496035b
Mitya <techwipes@gmail.com>
2020-11-06 16:00:00 +0500

    add handler for showNewAccountForm

--- src/main/java/org/techwipes/apartmentmanager/controller/AccountController.java
+++ src/main/java/org/techwipes/apartmentmanager/controller/AccountController.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.techwipes.apartmentmanager.model.Account;
 import org.techwipes.apartmentmanager.service.AccountServiceImpl;
 
 @Controller
@@ -18,5 +19,12 @@ public class AccountController {
     public String viewHomePage(Model model){
         model.addAttribute("listAccounts", accountService.getAllAccounts());
         return "index";
+    }
+    @GetMapping("/showNewAccountForm")
+    public String showNewAccountForm(Model model){
+        // create model attribute to bind form data
+        Account account = new Account();
+        model.addAttribute("account", account);
+        return "new account";
     }
 }
